2f7a590f45addb49dec6b5ff5390facdf647b12a
Mirwais Farahi <waisfarahi@gmail.com>
2022-07-13 21:41:23 +0500

    react generated source folder with component folder

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Navbar, NavbarBrand } from 'reactstrap';
+import './App.css';
+import Menu from './components/MenuComponent';
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu />
+      </div>
+  );
+}
+
+export default App;
